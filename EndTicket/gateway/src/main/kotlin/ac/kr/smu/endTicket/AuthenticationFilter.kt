022def46715ac6a42ab4bbf5a88ffdd3f6c58bcb
YouHojoon <dbghwns66@daum.net>
2024-04-04 01:05:45 +0900

    add: 게이트웨이 - 인증이 필요한 uri 접근 시 인증 필터 거치는 기능 추가

--- EndTicket/gateway/src/main/kotlin/ac/kr/smu/endTicket/AuthenticationFilter.kt
+++ EndTicket/gateway/src/main/kotlin/ac/kr/smu/endTicket/AuthenticationFilter.kt
@@ -5,26 +5,35 @@ import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.gateway.filter.GatewayFilter
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.ErrorResponse
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.awaitBodilessEntity
 import reactor.core.publisher.Mono
 
 @Component
-class AuthenticationFilter: AbstractGatewayFilterFactory<AuthenticationFilter.Config>() {
+class AuthenticationFilter(
+    private val webClientBuilder: WebClient.Builder
+): AbstractGatewayFilterFactory<AuthenticationFilter.Config>(Config::class.java) {
 
     override fun apply(config: Config): GatewayFilter {
         return GatewayFilter { exchange, chain ->
-            if (exchange.request.uri.toString() in config.whitelist){
+            if (exchange.request.uri.toString() in config.whiteList){
                 return@GatewayFilter chain.filter(exchange)
             }
 
-            val token = exchange.request.headers.getFirst("Authorization") ?: return@GatewayFilter denyRequest(exchange.response, HttpStatus.UNAUTHORIZED, "access 토큰이 없습니다.")
+            val token = exchange.request.headers.getFirst("Authorization")
+                ?: return@GatewayFilter denyRequest(
+                    response = exchange.response,
+                    status = HttpStatus.UNAUTHORIZED,
+                    body = "access 토큰이 없습니다.".toByteArray())
 
-            WebClient
-                .create("http://auth")
+            webClientBuilder
+                .baseUrl("http://auth/auth/validation")
+                .build()
                 .post()
                 .header("Authorization", token)
                 .retrieve()
@@ -33,26 +42,20 @@ class AuthenticationFilter: AbstractGatewayFilterFactory<AuthenticationFilter.Co
                     chain.filter(exchange)
                 }
                 .onErrorResume {
-                    if (it is WebClientResponseException){
-                        denyRequest(exchange.response,
-                            HttpStatus.valueOf(it.statusCode.value()), it.message)
-                    }
-                    else{
-                        denyRequest(exchange.response, HttpStatus.UNAUTHORIZED, it.message)
-                    }
+                    if (it is WebClientResponseException)
+                       denyRequest(exchange.response, HttpStatus.valueOf(it.statusCode.value()), it.responseBodyAsByteArray)
+                    else
+                        denyRequest(exchange.response, HttpStatus.UNAUTHORIZED, it.message?.toByteArray())
                 }
         }
     }
 
-    private fun denyRequest(response: ServerHttpResponse, status: HttpStatus, message: String?): Mono<Void>{
-        val body = ObjectMapper().writeValueAsString(mapOf("message" to message, "code" to status.value()))
+    private fun denyRequest(response: ServerHttpResponse, status: HttpStatus, body: ByteArray?): Mono<Void>{
         response.statusCode = status
-        return response.writeWith {
-            it.onNext(
-                response.bufferFactory().wrap(body.toByteArray())
-            )
-        }
+        response.headers.contentType = MediaType.APPLICATION_JSON
+
+        return response.writeWith(Mono.just(response.bufferFactory().wrap(body ?: ByteArray(0))))
     }
 
-    data class Config(val whitelist: List<String>)
+    data class Config(val whiteList: List<String>)
 }
