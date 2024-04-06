aae4f68a033f1747a44490a32ef5f842f38c0224
YouHojoon <dbghwns66@daum.net>
2024-04-06 19:40:07 +0900

    remove: WebClient 관련 코드 제거

--- EndTicket/gateway/src/main/kotlin/ac/kr/smu/endTicket/AuthorizationFilter.kt
+++ EndTicket/gateway/src/main/kotlin/ac/kr/smu/endTicket/AuthorizationFilter.kt
@@ -13,38 +13,18 @@ import reactor.core.publisher.Mono
 @Component
 class AuthorizationFilter(
     private val authService: AuthService,
-    private val webClientBuilder: WebClient.Builder
 ): AbstractGatewayFilterFactory<Any>() {
-    private val AUTHORIZATION_URL = "http://auth/auth/validation"
+
     override fun apply(config: Any): GatewayFilter {
         return GatewayFilter { exchange, chain ->
             val token = exchange.request.headers.getFirst("Authorization")
                 ?: return@GatewayFilter denyRequest(exchange.response, HttpStatus.UNAUTHORIZED, "access 토큰이 없습니다.".toByteArray())
             val response = authService.validationToken(token)
 
-            when(response.status){
-                200 -> chain.filter(exchange)
-                else -> {
-                    denyRequest(exchange.response, HttpStatus.valueOf(response.status))
-                }
-            }
-
-//            webClientBuilder
-//                .baseUrl(AUTHORIZATION_URL)
-//                .build()
-//                .post()
-//                .header("Authorization", token)
-//                .retrieve()
-//                .toBodilessEntity()
-//                .flatMap {
-//                    chain.filter(exchange)
-//                }
-//                .onErrorResume {
-//                    if (it is WebClientResponseException)
-//                       denyRequest(exchange.response, HttpStatus.valueOf(it.statusCode.value()), it.responseBodyAsByteArray)
-//                    else
-//                        denyRequest(exchange.response, HttpStatus.UNAUTHORIZED, it.message?.toByteArray())
-//                }
+            if(response.isValid)
+                chain.filter(exchange)
+            else
+                denyRequest(exchange.response, HttpStatus.valueOf(response.status), response.message.toByteArray())
         }
     }
 
--- EndTicket/gateway/src/main/kotlin/ac/kr/smu/endTicket/GatewayApplication.kt
+++ EndTicket/gateway/src/main/kotlin/ac/kr/smu/endTicket/GatewayApplication.kt
@@ -4,20 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
-import org.springframework.context.annotation.Bean
-import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootApplication
 @EnableDiscoveryClient
-class GatewayApplication{
-    @Bean
-    @LoadBalanced
-    fun loadBalancedWebClient(): WebClient.Builder{
-        return WebClient.builder()
-    }
-}
-
+class GatewayApplication
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
