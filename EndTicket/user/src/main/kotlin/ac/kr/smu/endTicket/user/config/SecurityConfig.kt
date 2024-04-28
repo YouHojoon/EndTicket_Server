3376269246a13150b8fd0ee5b09e213d6e84ac0e
YouHojoon <dbghwns66@daum.net>
2024-04-29 01:42:36 +0900

    fix: user 서버 gateway에서 온 요청만 허용하도록 수정

--- EndTicket/user/src/main/kotlin/ac/kr/smu/endTicket/user/config/SecurityConfig.kt
+++ EndTicket/user/src/main/kotlin/ac/kr/smu/endTicket/user/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package ac.kr.smu.endTicket.user.config
 
+import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -7,10 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.util.matcher.IpAddressMatcher
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val discoveryClient: DiscoveryClient
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain{
@@ -21,7 +25,10 @@ class SecurityConfig {
             csrf { disable() }
             formLogin { disable() }
             authorizeRequests {
-                authorize(anyRequest, permitAll)
+                discoveryClient.getInstances("gateway").forEach {
+                    authorize(IpAddressMatcher(it.host), permitAll)
+                }
+                authorize(anyRequest, denyAll)
             }
         }
 
