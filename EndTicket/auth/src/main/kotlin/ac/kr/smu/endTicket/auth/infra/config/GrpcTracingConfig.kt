6ad2fb2fbfdd11bcfd13887f215241dbf935bdaa
YouHojoon <dbghwns66@daum.net>
2024-04-29 02:05:54 +0900

    add: 분산 추적을 위한 grpc ServerIntercpetor 추가

--- EndTicket/auth/src/main/kotlin/ac/kr/smu/endTicket/auth/infra/config/GrpcTracingConfig.kt
+++ EndTicket/auth/src/main/kotlin/ac/kr/smu/endTicket/auth/infra/config/GrpcTracingConfig.kt
@@ -5,13 +5,21 @@ import net.devh.boot.grpc.client.interceptor.GrpcGlobalClientInterceptor
 import org.springframework.context.annotation.Configuration
 import brave.grpc.GrpcTracing
 import io.grpc.ClientInterceptor
+import io.grpc.ServerInterceptor
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor
 import org.springframework.context.annotation.Profile
 
 @Profile("!test")
 @Configuration(proxyBeanMethods = false)
 class GrpcTracingConfig {
     @GrpcGlobalClientInterceptor
-    fun tracingInterceptor(tracing: Tracing): ClientInterceptor{
+    fun tracingClientInterceptor(tracing: Tracing): ClientInterceptor{
         return GrpcTracing.create(tracing).newClientInterceptor()
     }
+
+    @GrpcGlobalServerInterceptor
+    fun tracingServerInterceptor(tracing: Tracing): ServerInterceptor{
+        return GrpcTracing.create(tracing).newServerInterceptor()
+    }
+
 }
