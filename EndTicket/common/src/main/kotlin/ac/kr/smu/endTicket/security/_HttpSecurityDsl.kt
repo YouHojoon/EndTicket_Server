e7562d80699039eb9d0dcdb0a630ae9b50043bea
YouHojoon <dbghwns66@daum.net>
2024-05-11 03:12:10 +0900

    add: 티켓 스와이프, 스와이프 취소 API 문서 추가 - fix: Security 오류 수정

--- EndTicket/common/src/main/kotlin/ac/kr/smu/endTicket/security/_HttpSecurityDsl.kt
+++ EndTicket/common/src/main/kotlin/ac/kr/smu/endTicket/security/_HttpSecurityDsl.kt
@@ -25,7 +25,7 @@ fun HttpSecurityDsl.configLogin(){
  * Swagger 관련 요청을 모두 허용하도록 설정한다.
  */
 fun HttpSecurityDsl.permitAllSwaggerRequest(){
-    authorizeRequests {
+    authorizeHttpRequests {
         authorize("/docs/**", permitAll)
         authorize("/swagger-ui/**",permitAll)
         authorize("/api-docs/**",permitAll)
@@ -56,7 +56,7 @@ fun HttpSecurityDsl.baseExceptionHandling(){
 fun HttpSecurityDsl.permitOnlyWhitelistRequest(whitelist: List<String>){
     authorizeHttpRequests {
         whitelist.forEach {
-            authorize(IpAddressMatcher(it), authenticated)
+            authorize(IpAddressMatcher(it), permitAll)
         }
         authorize(anyRequest, denyAll)
     }
