a98580b6f7a7c5b34b538e9f8affda59bee5d4a8
YouHojoon <dbghwns66@daum.net>
2024-06-04 01:12:16 +0900

    add: 상상해보기 수정 기능 추가

--- EndTicket/futureMe/src/main/kotlin/ac/kr/smu/endTicket/futureMe/service/ImaginationService.kt
+++ EndTicket/futureMe/src/main/kotlin/ac/kr/smu/endTicket/futureMe/service/ImaginationService.kt
@@ -1,10 +1,12 @@
 package ac.kr.smu.endTicket.futureMe.service
 
+import ac.kr.smu.endTicket.futureMe.domain.imagination.exception.NotFoundImaginationException
 import ac.kr.smu.endTicket.futureMe.domain.imagination.model.Imagination
 import ac.kr.smu.endTicket.futureMe.domain.imagination.repository.ImaginationRepository
 import ac.kr.smu.endTicket.futureMe.ui.request.ImaginationRequest
 import ac.kr.smu.endTicket.futureMe.ui.response.ImaginationResponse
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class ImaginationService(
@@ -16,7 +18,7 @@ class ImaginationService(
     /**
      * 상상해보기를 생성하는 메소드
      * @param request 생성 요청
-     * @param userID 생성을 요청한 사용자
+     * @param userID 생성을 요청한 사용자의 id
      * @return 생성된 상상해보기
      * @throws IllegalStateException 최대 개수 이상으로 생성 시도할 시
      */
@@ -26,4 +28,19 @@ class ImaginationService(
         ImaginationResponse.from(repo.save(Imagination.from(request,userID)))
     }
 
+    /**
+     * 상상해보기를 수정하는 메소드
+     * @param request 수정 요청
+     * @param id 상상해보기의 id
+     * @param userID 수정을 요청한 사용자의 id
+     * @return 수정된 상상해보기
+     * @throws NotFoundImaginationException 상상해보기가 존재하지 않을 시
+     */
+    @Throws(NotFoundImaginationException::class)
+    fun updateImagination(request: ImaginationRequest, id: Long, userID: Long): ImaginationResponse{
+        val imagination = repo.findById(id).getOrNull() ?: throw NotFoundImaginationException(id)
+
+        imagination.update(request,userID)
+        return ImaginationResponse.from(imagination)
+    }
 }
--- EndTicket/futureMe/src/test/kotlin/ac/kr/smu/endTicket/futureMe/ImaginationServiceTest.kt
+++ EndTicket/futureMe/src/test/kotlin/ac/kr/smu/endTicket/futureMe/ImaginationServiceTest.kt
@@ -1,7 +1,10 @@
 package ac.kr.smu.endTicket.futureMe
 
+import ac.kr.smu.endTicket.futureMe.domain.imagination.exception.NotFoundImaginationException
+import ac.kr.smu.endTicket.futureMe.domain.imagination.model.Imagination
 import ac.kr.smu.endTicket.futureMe.domain.imagination.repository.ImaginationRepository
 import ac.kr.smu.endTicket.futureMe.service.ImaginationService
+import ac.kr.smu.endTicket.futureMe.ui.request.ImaginationRequest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -12,6 +15,9 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
+import javax.swing.text.html.Option
+import kotlin.test.assertEquals
 
 @ExtendWith(MockitoExtension::class)
 class ImaginationServiceTest(
@@ -33,5 +39,42 @@ class ImaginationServiceTest(
             .thenReturn(6)
 
         assertThrows<IllegalStateException> {  service.createImagination(request, USER_ID)}
+    }
+    @Test
+    @DisplayName("상상해보기 수정 테스트")
+    fun given_request_when_updateImagination_then_returnUpdatedImagination(){
+        val imagination = Imagination.from(request, USER_ID)
+        val request = ImaginationRequest("xx", "zzz", Imagination.Color.GRAY2)
+        Mockito
+            .`when`(repo.findById(imagination.id))
+            .thenReturn(Optional.of(imagination))
+
+        val updated = service.updateImagination(request, imagination.id, USER_ID)
+
+        assertEquals(request.behavior, updated.behavior)
+        assertEquals(request.target, updated.target)
+        assertEquals(request.color, updated.color)
+    }
+
+    @Test
+    @DisplayName("소유자가 아닌 사용자의 상상해보기 테스트")
+    fun given_userWhoNotOwner_when_updateImagination_then_throwIllegalStateException(){
+        val imagination = Imagination.from(request, USER_ID)
+
+        Mockito.`when`(repo.findById(imagination.id))
+            .thenReturn(Optional.of(imagination))
+
+        val request = ImaginationRequest("xx", "zzz", Imagination.Color.GRAY2)
+        assertThrows<IllegalStateException> {  service.updateImagination(request, imagination.id, 2L)}
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상상해보기 수정 테스트")
+    fun given_notExistImagination_when_updateImagination_then_throwNotFoundImaginationException(){
+        Mockito.`when`(repo.findById(Mockito.anyLong()))
+            .thenReturn(Optional.empty())
+
+        val request = ImaginationRequest("xx", "zzz", Imagination.Color.GRAY2)
+        assertThrows<NotFoundImaginationException> {  service.updateImagination(request,1L, USER_ID)}
     }
 }
