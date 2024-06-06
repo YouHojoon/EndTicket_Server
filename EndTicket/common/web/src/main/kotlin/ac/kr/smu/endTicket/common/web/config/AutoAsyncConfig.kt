87d526032879fd14566a2a0680f7c47c2156248a
YouHojoon <dbghwns66@daum.net>
2024-06-07 02:00:05 +0900

    fix: AutoAsyncConfig 어노테이션 정보 못가져오는 문제 해결

--- EndTicket/common/web/src/main/kotlin/ac/kr/smu/endTicket/common/web/config/AutoAsyncConfig.kt
+++ EndTicket/common/web/src/main/kotlin/ac/kr/smu/endTicket/common/web/config/AutoAsyncConfig.kt
@@ -3,6 +3,7 @@ package ac.kr.smu.endTicket.common.web.config
 import ac.kr.smu.endTicket.common.web.annotation.EnableAutoAsyncConfig
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
+import org.springframework.core.annotation.AnnotationUtils
 import kotlin.reflect.full.findAnnotation
 
 
@@ -12,7 +13,10 @@ class AutoAsyncConfig(
     @Bean
     fun asyncConfig(): AsyncConfig {
         val beans = applicationContext.getBeansWithAnnotation(EnableAutoAsyncConfig::class.java)
-        val corePoolSize = beans.mapNotNull { it.value::class.findAnnotation<EnableAutoAsyncConfig>() }.maxOf { it.corePoolSize }
+        val corePoolSize = beans
+            .mapNotNull { AnnotationUtils.findAnnotation(it.value.javaClass, EnableAutoAsyncConfig::class.java) }
+            .maxOf { it.corePoolSize}
+        
         return AsyncConfig(corePoolSize)
     }
 }
