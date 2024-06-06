efe7c2b30736cee4e75b7725a9d3ddbd6effa980
YouHojoon <dbghwns66@daum.net>
2024-06-06 19:05:03 +0900

    add: 비동기 자동설정 및 테스트코드 추가

---
+++ EndTicket/common/web/src/main/kotlin/ac/kr/smu/endTicket/common/web/config/AsyncConfig.kt
@@ -0,0 +1,20 @@
+package ac.kr.smu.endTicket.common.web.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+
+
+@EnableAsync
+class AsyncConfig(
+    private val corePoolSize: Int
+){
+    @Bean
+    fun threadPoolTaskExecutor(): ThreadPoolTaskExecutor = ThreadPoolTaskExecutor().apply {
+        // 참고 : https://medium.com/@greg.shiny82/트랜잭셔널-아웃박스-패턴의-실제-구현-사례-29cm-0f822fc23edb
+        this.corePoolSize = this@AsyncConfig.corePoolSize //파티션의 개수만큼 할당
+        setAllowCoreThreadTimeOut(true)
+        setWaitForTasksToCompleteOnShutdown(true)
+        setAwaitTerminationSeconds(10)
+    }
+}
