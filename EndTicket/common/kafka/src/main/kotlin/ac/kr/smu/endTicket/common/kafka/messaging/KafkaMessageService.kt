f4ef5685b349773c870e7166ddab4555170086b9
YouHojoon <dbghwns66@daum.net>
2024-06-14 10:57:19 +0900

    add: 공통 모듈 카프카 설정을 활성화하는 어노테이션 추가 fix: 카프카 메시지 서비스 @Componet 제거 후 자동 설정에서 @Bean으로 등록하도록 변경 fix: 공통모듈 webflux에 web 의존성 추가 remove: 공통 모듈 webflux response 패키지 제거

--- EndTicket/common/kafka/src/main/kotlin/ac/kr/smu/endTicket/common/kafka/messaging/KafkaMessageService.kt
+++ EndTicket/common/kafka/src/main/kotlin/ac/kr/smu/endTicket/common/kafka/messaging/KafkaMessageService.kt
@@ -5,25 +5,28 @@ import org.springframework.stereotype.Component
 import java.util.concurrent.CompletableFuture
 
 /**
- * 상상해보기 완료 이벤트의 메시지 발행을 담당하는 서비스
- * @property kafkaTemplate 카프카 메시지 발행을 위한 클래스
+ * 카프카 메시지 전송을 담당하는 서비스
+ * @property kafkaTemplate 카프카 메시지 전송을 위한 객체
+ * @property K 카프카 메시지 키의 타입
+ * @property V 카프카 메시지 값의 타입
  */
-@Component
 class KafkaMessageService<K: Any,V>(
     private val kafkaTemplate: KafkaTemplate<K, V>
 ) {
 
     /**
-     * 상상해보기 이벤트 완료 메시지를 전송하는 메소드
-     * @param message 메시지를 발행할 이벤트
+     * 카프카 메시지를 전송하는 메소드
+     * @param topic 카프카 토픽
+     * @param message 카프카 메시지
      * @return 메시지에 대한 CompletableFuture
      */
     fun send(topic: String, message: KafkaMessage<K,V>) = kafkaTemplate.send(topic,message.key, message.payload)
 
 
     /**
-     * 상상해보기 이벤트 완료 메시지들을 전송하는 메소드
-     * @param messages 메시지를 전송할 이벤트들
+     * 카프카 메시지들을 전송하는 메소드
+     * @param topic 카프카 토픽
+     * @param messages 카프카 메시지
      * @return 메시지들에 대한 CompletableFuture
      */
     fun send(topic: String, messages: Collection<KafkaMessage<K,V>>): Collection<CompletableFuture<SendResult<K, V>>> = messages.map { send(topic, it) }
--- EndTicket/common/webflux/build.gradle.kts
+++ EndTicket/common/webflux/build.gradle.kts
@@ -1,4 +1,8 @@
 dependencies {
     api("org.springframework.boot:spring-boot-starter-webflux")
     api("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
+    api(project(":common:web")){
+        exclude("org.springframework.boot","spring-boot-starter-web")
+        exclude("org.springdoc","springdoc-openapi-starter-webmvc-ui")
+    }
 }
