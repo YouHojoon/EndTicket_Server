bdb75825499f682b4ebd43085c50c9c8834b90eb
YouHojoon <dbghwns66@daum.net>
2024-06-14 12:47:32 +0900

    add: 미래의 나 jacoco task 추가

--- EndTicket/futureMe/build.gradle.kts
+++ EndTicket/futureMe/build.gradle.kts
@@ -1,4 +1,3 @@
-
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation(project(":common:kafka"))
@@ -9,4 +8,72 @@ dependencies {
     testRuntimeOnly("com.h2database:h2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test{
+    finalizedBy("jacocoTestReport")
+}
+
+tasks.jacocoTestReport{
+    reports{
+        html.required = true
+    }
+    finalizedBy("jacocoTestCoverageVerification")
+
+    classDirectories.setFrom(
+        files(classDirectories.files.map {
+            fileTree(it) {
+                exclude(
+                    "**/infra/*",
+                    "**/*Application*",
+                    "**/domain/*",
+                    "**/response/*",
+                    "**/request/*",
+                    "**/TicketCompletionEventConsumeService*",
+                    "**/listener/*",
+                    "**/job/*"
+                )
+            }
+        })
+    )
+}
+
+
+tasks.jacocoTestCoverageVerification {
+    enabled = true
+
+    violationRules {
+        rule {
+            excludes = listOf(
+                "**.infra.**",
+                "*Application*",
+                "**.domain.*",
+                "**.job.*",
+                "**.TicketCompletionEventConsumeService*",
+                "**.listener.*",
+            )
+
+            element = "CLASS"
+
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = 0.90.toBigDecimal()
+            }
+
+            // 라인 커버리지를 최소한 80% 만족시켜야 합니다.
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = 0.80.toBigDecimal()
+            }
+
+            // 빈 줄을 제외한 코드의 라인수를 최대 200라인으로 제한합니다.
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = 200.toBigDecimal()
+            }
+        }
+    }
 }
