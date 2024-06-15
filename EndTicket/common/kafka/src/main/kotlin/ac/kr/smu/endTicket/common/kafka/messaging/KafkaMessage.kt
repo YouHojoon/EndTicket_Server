10ab359201bfa9d93ff14e3d933487d6eb7d855d
YouHojoon <dbghwns66@daum.net>
2024-06-15 10:49:15 +0900

    fix: KafkaMessage data 클래스로 변경

--- EndTicket/common/kafka/src/main/kotlin/ac/kr/smu/endTicket/common/kafka/messaging/KafkaMessage.kt
+++ EndTicket/common/kafka/src/main/kotlin/ac/kr/smu/endTicket/common/kafka/messaging/KafkaMessage.kt
@@ -5,7 +5,7 @@ package ac.kr.smu.endTicket.common.kafka.messaging
  * @property key 카프카 메시지의 키
  * @property payload 카프카 메시지의 페이로드
  */
-open class KafkaMessage<out K:Any, out V>(
+data class KafkaMessage<out K:Any, out V>(
     val key: K,
     val payload: V
 )
