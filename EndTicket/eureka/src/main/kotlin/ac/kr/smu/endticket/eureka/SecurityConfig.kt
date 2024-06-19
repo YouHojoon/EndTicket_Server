1de5af948cbe7f65c92fd1ef9ecf59c1b5626e7a
YouHojoon <dbghwns66@daum.net>
2024-06-19 12:49:09 +0900

    move: 기본 패키지명 변경

--- EndTicket/eureka/src/main/kotlin/ac/kr/smu/endticket/eureka/SecurityConfig.kt
+++ EndTicket/eureka/src/main/kotlin/ac/kr/smu/endticket/eureka/SecurityConfig.kt
@@ -1,4 +1,4 @@
-package ac.kr.smu.endTicket
+package ac.kr.smu.endticket.eureka
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
--- EndTicket/eureka/src/main/kotlin/ac/kr/smu/endticket/eureka/WhitelistProperties.kt
+++ EndTicket/eureka/src/main/kotlin/ac/kr/smu/endticket/eureka/WhitelistProperties.kt
@@ -1,4 +1,4 @@
-package ac.kr.smu.endTicket
+package ac.kr.smu.endticket.eureka
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
