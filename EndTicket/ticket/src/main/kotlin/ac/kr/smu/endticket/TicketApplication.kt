ae860438c4f81f09a9680d007d5cc5e2308aee55
YouHojoon <dbghwns66@daum.net>
2024-07-05 02:13:35 +0900

    fix: 티켓서버 비동기 자동설정 사용하도록 수정

--- EndTicket/ticket/src/main/kotlin/ac/kr/smu/endticket/TicketApplication.kt
+++ EndTicket/ticket/src/main/kotlin/ac/kr/smu/endticket/TicketApplication.kt
@@ -4,14 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
 @EnableJpaAuditing
-@EnableAsync
 class TicketApplication
 
 fun main(args: Array<String>) {
