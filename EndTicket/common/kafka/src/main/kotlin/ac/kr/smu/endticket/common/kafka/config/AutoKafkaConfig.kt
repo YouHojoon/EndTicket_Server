74b5e5e14a851ef62a3eddd5ae34f1312ee8be8a
YouHojoon <dbghwns66@daum.net>
2024-07-06 02:41:56 +0900

    add: 공통모듈 카프카 DLTHanlder와 재시도 설정 추가

--- EndTicket/common/kafka/src/main/kotlin/ac/kr/smu/endticket/common/kafka/config/AutoKafkaConfig.kt
+++ EndTicket/common/kafka/src/main/kotlin/ac/kr/smu/endticket/common/kafka/config/AutoKafkaConfig.kt
@@ -1,10 +1,16 @@
 package ac.kr.smu.endticket.common.kafka.config
 
 import KafkaMessageService
+import ac.kr.smu.endticket.common.kafka.handler.DLTHandler
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.retrytopic.DltStrategy
+import org.springframework.kafka.retrytopic.RetryTopicConfiguration
+import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy
+import org.springframework.kafka.support.EndpointHandlerMethod
 
 @EnableKafka
 class AutoKafkaConfig {
@@ -12,4 +18,20 @@ class AutoKafkaConfig {
     @ConditionalOnMissingBean(KafkaMessageService::class)
     fun <K : Any, V> kafkaMessageService(kafkaTemplate: KafkaTemplate<K, V>) =
         KafkaMessageService(kafkaTemplate.also { it.setObservationEnabled(true) })
+
+    @Bean
+    @ConditionalOnMissingBean(RetryTopicConfiguration::class)
+    fun retryableTopic(kafkaTemplate: KafkaTemplate<String, Any>) =
+        RetryTopicConfigurationBuilder
+            .newInstance()
+            .maxAttempts(3)
+            .exponentialBackoff(
+                5 * 1000,
+                2.0,
+                20 * 1000,
+            ).autoCreateTopics(true, 3, 3)
+            .setTopicSuffixingStrategy(TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
+            .dltProcessingFailureStrategy(DltStrategy.ALWAYS_RETRY_ON_ERROR)
+            .dltHandlerMethod(EndpointHandlerMethod(DLTHandler::class.java, "handle"))
+            .create(kafkaTemplate)
 }
