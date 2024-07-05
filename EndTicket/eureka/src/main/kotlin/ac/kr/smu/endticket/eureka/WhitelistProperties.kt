56cee470d28d5f75065c75a29e75b0f91f766fb6
YouHojoon <dbghwns66@daum.net>
2024-07-05 19:52:09 +0900

    fix: 유레카 서버 화이트리스트 제거

--- EndTicket/eureka/src/main/kotlin/ac/kr/smu/endticket/eureka/WhitelistProperties.kt
+++
@@ -1,6 +0,0 @@
-package ac.kr.smu.endticket.eureka
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties("whitelist")
-data class WhitelistProperties(val addresses: List<String>)
