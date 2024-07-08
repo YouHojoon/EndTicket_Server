69e0c32607affce568937e1c70b4da76014616bc
YouHojoon <dbghwns66@daum.net>
2024-07-08 22:46:20 +0900

    fix: JpaAuditing 활성화, BindExceptionAdvice 적용

--- EndTicket/user/src/main/kotlin/ac/kr/smu/endticket/UserApplication.kt
+++ EndTicket/user/src/main/kotlin/ac/kr/smu/endticket/UserApplication.kt
@@ -1,14 +1,16 @@
 package ac.kr.smu.endticket
 
-import ac.kr.smu.endticket.common.web.annotation.EnableAutoAsyncConfig
+import ac.kr.smu.endticket.common.web.aop.BindExceptionAdvice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Import
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@Import(BindExceptionAdvice::class)
 class UserApplication
 
 fun main(args: Array<String>) {
---
+++ EndTicket/user/src/main/kotlin/ac/kr/smu/endticket/user/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package ac.kr.smu.endticket.user.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig
